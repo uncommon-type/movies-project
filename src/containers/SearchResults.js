574000ff0c0e3b5b63b46d0c681256f78535e5ea
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 00:22:59 +0300

    Add a call to a serverless function with await fetch and mark response as json

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -2,7 +2,18 @@ import { Fragment } from "react";
 import MovieList from "../components/MovieList";
 import PlayList from "../components/PlayList";
 
-const SearchResults = () => {
+const SearchResults = ({ searchTerm }) => {
+  const getMovies = async (searchTerm, searchType = "text") => {
+    try {
+      const res = await fetch(
+        `/.netlify/functions/movies?searchTerm=${searchTerm}&searchType=${searchType}`
+      );
+      return await res.json();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <Fragment>
       <MovieList />
