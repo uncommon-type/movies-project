87ebeded8c6900a8da6ec58204e29f75c879ef4e
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 22:23:36 +0300

    Update jsx

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const ErrorMessage = () => (
-  <aside class="search__error">
-    <h2 class="search__error__title">Not Found</h2>
-  </aside>
+  <div className="search__error">
+    <h2 className="search__error__title">Not Found</h2>
+  </div>
 );
 
 export default ErrorMessage;
