7c4f5897bc9980a7b2938267999451c537b937c0
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 13:43:09 +0300

    Map over searchResults to generate an array of MovieListItem components

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,9 +6,15 @@ const MovieList = ({ searchTerm, searchResults }) => (
     <div className="movie-list-group__inner">
       <h3 className="movie-list__title text-700">Results for: {searchTerm}</h3>
       <ul id="movie-list">
-        <MovieListItem className="movie-item" />
-        <MovieListItem className="movie-item" />
-        <MovieListItem className="movie-item" />
+        {searchResults.map(({ imdbID: id, Title, Year }) => (
+          <MovieListItem
+            key={id}
+            className="movie-item"
+            id={id}
+            title={Title}
+            year={Year}
+          />
+        ))}
       </ul>
     </div>
   </section>
