fa1973cf6d48bd746acf21f6d87ebf64e03970fa
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 10:20:13 +0300

    Give input field a value prop

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -5,7 +5,13 @@ const SearchBar = ({ onSearch }) => {
   return (
     <div className="app__search wrapper">
       <form className="app__search__controls" action="/search" method="get">
-        <input type="text" name="q" id="searchBar" placeholder="Movie Title" />
+        <input
+          value={searchTerm}
+          type="text"
+          name="q"
+          id="searchBar"
+          placeholder="Movie Title"
+        />
       </form>
     </div>
   );
