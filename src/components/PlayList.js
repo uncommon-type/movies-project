2164ba7fee8b3aa12a20f658f7fdab94e2ba2869
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 14:34:32 +0300

    Map over playlist to generate array of playListItems

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -1,12 +1,19 @@
 import React from "react";
 import PlayListItem from "./PlayListItem";
 
-const PlayList = () => (
+const PlayList = ({ playList }) => (
   <section className="playlist-group">
     <h3 className="playlist__title text-700">Playlist</h3>
     <ul id="playlist__container">
-      <PlayListItem className="playlist-item" />
-      <PlayListItem className="playlist-item" />
+      {playList.map(({ id, title, year }) => (
+        <PlayListItem
+          key={id}
+          className="playlist-item"
+          id={id}
+          title={title}
+          year={year}
+        />
+      ))}
     </ul>
   </section>
 );
