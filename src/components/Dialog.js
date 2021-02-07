d8d32a968aee045dd20866a07864507f6967ea04
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-07 23:45:12 +0300

    Add Dialog component

---
+++ src/components/Dialog.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dialog = () => (
+  <div id="dialog">
+    <div className="dialog__inner">
+      <p id="dialog-content">🎉 Yay! You've selected 3 movies</p>
+      <button id="dialog-content__close-button">Okay</button>
+    </div>
+  </div>
+);
+
+export default Dialog;
