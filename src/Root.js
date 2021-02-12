3b50a64df68c26676fd6381f19769f4fe01128cd
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-12 21:45:52 +0300

    Import Router, Route from react-router-dom

--- src/Root.js
+++ src/Root.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SearchBar from "./containers/SearchBar";
 import Header from "./components/Header";
 import SearchResults from "./containers/SearchResults";
