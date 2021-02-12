3e929f27d5f8b06cb931ce6804e7472104fb5cc5
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-02-12 22:29:21 +0300

    Remove useEffect import

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 
 const SearchBar = () => {
