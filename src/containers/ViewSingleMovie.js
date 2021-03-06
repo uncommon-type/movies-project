73f5eb0fa2c348b31bdbb07fb27b44e8289e64b6
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-06 19:58:12 +0300

    Add useEffect hook

--- src/containers/ViewSingleMovie.js
+++ src/containers/ViewSingleMovie.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 
 const ViewSingleMovie = () => {
   const [movie, setMovie] = useState([]);
+
+  useEffect(() => {});
 };
 
 export default ViewSingleMovie;
