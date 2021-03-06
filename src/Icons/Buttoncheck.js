492fa1e4e0a849c58888cca887405b8ef30bfbd8
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-07 00:20:38 +0300

    Add React icon component

---
+++ src/Icons/Buttoncheck.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function SvgButtoncheck(props) {
+  return (
+    <svg fill="#fff" viewBox="0 0 100 100" aria-hidden="true" {...props}>
+      <path d="M39.363 79L16 55.49l11.347-11.419L39.694 56.49 72.983 23 84 34.085 39.363 79z" />
+    </svg>
+  );
+}
+
+export default SvgButtoncheck;
---
+++ src/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as Buttoncheck } from './Buttoncheck'
+export { default as Firework } from './Firework'
+export { default as Logo } from './Logo'
+export { default as Star } from './Star'
