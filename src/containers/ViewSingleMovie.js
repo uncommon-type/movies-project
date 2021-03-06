65eb20cb28e0247713bd64b4664a019ffc2a9a7f
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-06 20:00:10 +0300

    In useEffect hook, call getMovies(), add dependency array to make side-effect run whenever its values get updated

--- src/containers/ViewSingleMovie.js
+++ src/containers/ViewSingleMovie.js
@@ -3,7 +3,21 @@ import React, { useEffect, useState } from "react";
 const ViewSingleMovie = () => {
   const [movie, setMovie] = useState([]);
 
-  useEffect(() => {});
-};
+  useEffect(() => {
+    const getMovies = async (id, searchType) => {
+      try {
+        const res = await fetch(
+          `/.netlify/functions/search?searchTerm=${id}&searchType=${searchType}`
+        );
+
+        const data = await res.json();
+        setMovie(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getMovies(id, "id");
+  }, [id, setMovie]);
 
 export default ViewSingleMovie;
