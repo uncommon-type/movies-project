881fcfb8fb59ca037b2ea7ade8189a0e898730ae
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 19:54:23 +0300

    Remove onRemove prop

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PlayListItem from "./PlayListItem";
 
-const PlayList = ({ playList, onRemove }) => (
+const PlayList = ({ playList }) => (
   <section className="playlist-group">
     <h3 className="playlist__title text-700">Playlist</h3>
     <ul id="playlist__container">
@@ -12,7 +12,6 @@ const PlayList = ({ playList, onRemove }) => (
           id={id}
           title={title}
           year={year}
-          onRemove={onRemove}
         />
       ))}
     </ul>
