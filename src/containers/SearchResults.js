df1308f32b89ce9f511f14ff3c86bd852a8d5d7a
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 20:04:24 +0300

    Import useContext hook from react

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useContext } from "react";
 
 import { useLocation } from "react-router-dom";
 import { parse } from "query-string";
