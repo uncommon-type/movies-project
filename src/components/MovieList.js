96fc1faf643966c5985a31ad24a1ced9beac238c
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 18:37:28 +0300

    Remove onAdd prop

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,13 +1,7 @@
 import React from "react";
 import MovieListItem from "./MovieListItem";
 
-const MovieList = ({
-  searchTerm,
-  searchResults,
-  onAdd,
-  checkIfAdded,
-  onRemove,
-}) => (
+const MovieList = ({ searchTerm, searchResults, checkIfAdded, onRemove }) => (
   <section className="movie-list-group">
     <div className="movie-list-group__inner">
       <h3 className="movie-list__title text-700">Results for: {searchTerm}</h3>
@@ -19,7 +13,6 @@ const MovieList = ({
             id={id}
             title={Title}
             year={Year}
-            onAdd={onAdd}
             isAdded={checkIfAdded(id)}
             onRemove={onRemove}
           />
