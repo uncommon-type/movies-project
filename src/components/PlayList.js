f4cc9345f8ff97eda8f8490ab84b78ccf24a4000
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 20:27:14 +0300

    Get appState from useContext hook

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -4,21 +4,25 @@ import StateContext from "../StateContext";
 
 import PlayListItem from "./PlayListItem";
 
-const PlayList = () => (
-  <section className="playlist-group">
-    <h3 className="playlist__title text-700">Playlist</h3>
-    <ul id="playlist__container">
-      {playList.map(({ id, title, year }) => (
-        <PlayListItem
-          key={id}
-          className="playlist-item"
-          id={id}
-          title={title}
-          year={year}
-        />
-      ))}
-    </ul>
-  </section>
-);
+const PlayList = () => {
+  const appState = useContext(StateContext);
+
+  return (
+    <section className="playlist-group">
+      <h3 className="playlist__title text-700">Playlist</h3>
+      <ul id="playlist__container">
+        {appState.playList.map(({ id, title, year }) => (
+          <PlayListItem
+            key={id}
+            className="playlist-item"
+            id={id}
+            title={title}
+            year={year}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default PlayList;
