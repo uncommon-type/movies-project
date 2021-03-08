c066de75200ff7a5dd23c8ffd0d9abfdf9d2ec25
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 18:57:17 +0300

    Remove component

--- src/components/RemoveButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const RemoveButton = ({ id, onRemove }) => (
-  <button onClick={() => onRemove(id)} type="button" className="btn">
-    <span className="btn__txt">Remove</span>
-  </button>
-);
-
-export default RemoveButton;
