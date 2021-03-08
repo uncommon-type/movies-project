7fceaa8b09868e3f976aa6c2b6b4d239f96bf05b
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 18:50:07 +0300

    Get appDispatch function from useContext hook

--- src/components/PlayListItem.js
+++ src/components/PlayListItem.js
@@ -5,18 +5,22 @@ import DispatchContext from "../DispatchContext";
 import RemoveButton from "./RemoveButton";
 import StarIcon from "../Icons/Star.js";
 
-const PlayListItem = ({ id, title, year, onRemove }) => (
-  <li className="playlist__item">
-    <div className="playlist__item__body">
-      <div className="playlist-item__body__inner">
-        <StarIcon id="star" />
-        <p className="playlist__item__text" data-movie-title="">
-          {title} ({year})
-        </p>
+const PlayListItem = ({ id, title, year, onRemove }) => {
+  const appDispatch = useContext(DispatchContext);
+
+  return (
+    <li className="playlist__item">
+      <div className="playlist__item__body">
+        <div className="playlist-item__body__inner">
+          <StarIcon id="star" />
+          <p className="playlist__item__text" data-movie-title="">
+            {title} ({year})
+          </p>
+        </div>
+        <RemoveButton id={id} onRemove={onRemove} />
       </div>
-      <RemoveButton id={id} onRemove={onRemove} />
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default PlayListItem;
