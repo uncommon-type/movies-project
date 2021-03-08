08242176cfcffb01af8bb0ac3d259b639aec3d12
‘uncommon-type’ <uncommon.type@rambler.ru>
2021-03-08 19:55:03 +0300

    Remove onRemove prop

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieListItem from "./MovieListItem";
 
-const MovieList = ({ searchTerm, searchResults, checkIfAdded, onRemove }) => (
+const MovieList = ({ searchTerm, searchResults, checkIfAdded }) => (
   <section className="movie-list-group">
     <div className="movie-list-group__inner">
       <h3 className="movie-list__title text-700">Results for: {searchTerm}</h3>
@@ -14,7 +14,6 @@ const MovieList = ({ searchTerm, searchResults, checkIfAdded, onRemove }) => (
             title={Title}
             year={Year}
             isAdded={checkIfAdded(id)}
-            onRemove={onRemove}
           />
         ))}
       </ul>
